2026c4b3a0a611dd7c4675db38962bb0f83be3e5
mostafizaiub1990 <mostafizaiub1990@gmail.com>
2023-08-29 14:00:51 -0400

    HomeWork

--- src/hw4JavaVariables/AboutMe.java
+++ src/hw4JavaVariables/AboutMe.java
@@ -11,27 +11,17 @@ public class AboutMe {
 	public double myGrade;
 	public char myGender;
 	public boolean usCitizen;
-	//Constructor --> 
+
+	// Constructor -->
 	public AboutMe() {
 		System.out.println("This is all about us: ");
 	}
 
-	public void aboutMe() {
-		System.out.println("My name is " +myName + "\nMy age is " + myAge + "\nMy Apartment Rent is "
-				+ myApartmentRent + "\nMy Yarly Salary is " + myYearlySalary + "\nMy Bank Balance is "
-				+myBankBalance + "\nMy Height is " + myHeight + "\nMy Gender is " + myGender
-				+ "\nMy Grade is " +myGrade + "\nMy US Citizenship News Status is " + usCitizen);
-		System.out.println("hi my name is"  + myName);
-	}
-	
-	
-	public void aboutMe2() {
-		System.out.println("My name is " +myName + "\nMy age is " + myAge + "\nMy Apartment Rent is "
-				+ myApartmentRent + "\nMy Yarly Salary is " + myYearlySalary + "\nMy Bank Balance is "
-				+myBankBalance + "\nMy Height is " + myHeight + "\nMy Gender is " + myGender
-				+ "\nMy Grade is "
-				+ "" +myGrade + "\nMy US Citizenship News Status is " + usCitizen);
-		System.out.println("hi my name is"  + myName);
-	}
 
+	public void aboutMe() {
+		System.out.println("My name is " + myName + "\nMy age is " + myAge + "\nMy Apartment Rent is " + myApartmentRent
+				+ "\nMy Yarly Salary is " + myYearlySalary + "\nMy Bank Balance is " + myBankBalance + "\nMy Height is "
+				+ myHeight + "\nMy Gender is " + myGender + "\nMy Grade is " + myGrade
+				+ "\nMy US Citizenship News Status is " + usCitizen);
+	}
 }
--- src/hw4JavaVariables/AboutMeTest.java
+++ src/hw4JavaVariables/AboutMeTest.java
@@ -2,23 +2,31 @@ package hw4JavaVariables;
 
 public class AboutMeTest {
 	public static void main(String[] args) {
-		AboutMe kazi = new AboutMe();// constructors are used or intialiezed called when an object is created(ex. kazi, alex, alex1.....)
-		AboutMe alex = new AboutMe();
-		AboutMe alex1 = new AboutMe();
-		AboutMe alex2 = new AboutMe();
-		AboutMe alex3= new AboutMe();
-//		kazi.myName = "kazi"; // here initialized
-//		kazi.myAge = 30;
-//		kazi.myHeight=5.8f;
+		/* constructors are called when an object is created(Example:kazi, Alex) */
+		AboutMe kazi = new AboutMe();
+		AboutMe Alex = new AboutMe();
+		kazi.myName = "Kazi"; // here initialized
+		kazi.myAge = 33;
+		kazi.myApartmentRent = 32767;
+		kazi.myYearlySalary = 22372;
+		kazi.myBankBalance = 6465636;
+		kazi.myHeight = 5.6f;
+		kazi.myGrade = 3.46;
+		kazi.myGender = 'M';
+		kazi.usCitizen = false;
+
+		Alex.myName = "Alex";// here initialized
+		Alex.myAge = 32;
+		Alex.myApartmentRent = 31767;
+		Alex.myYearlySalary = 21372;
+		Alex.myBankBalance = 6965636;
+		Alex.myHeight = 5.10f;
+		Alex.myGrade = 2.46;
+		Alex.myGender = 'F';
+		Alex.usCitizen = false;
+
 		kazi.aboutMe();
-		kazi.aboutMe2();
-//		.,,,,,,
-//		
-//		alex.myAge..alex.myAge.
-//		
-//		aleex.aboutMe();
-//		
-		
+		Alex.aboutMe();
 	}
 
 }
